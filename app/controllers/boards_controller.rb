b536bc4ef26e0e7e52165d65131f29d745523937
USERNAME <shinozakiyume88@gmail.com>
2022-09-07 08:50:00 +0900

    52. 掲示板データの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,8 +4,18 @@ class BoardsController < ApplicationController
 
   def new
     @board = Board.new
+  end
 
-    
+  def create
+    Board.create(board_params)
+    binding.pry
+  end
+
+  private
+  # このprivateメソッドを呼ぶことでparamsの中にあるboardのキーの中のname、titile、bodyの値のみを取得する
+
+  def board_params
+    params.require(:board).permit(:name,:title,:body)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # restfull名書き方
   get 'boards',to: 'boards#index' 
   get 'boards/new',to: 'boards#new' 
+  post 'boards',to: 'boards#create' 
 						
 end
