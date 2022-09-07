23b27c81e7e690feb2ae7ad0da3ebeb1fd36b093
USERNAME <shinozakiyume88@gmail.com>
2022-09-07 10:21:25 +0900

    54. 日付データのフォーマット

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,9 +11,13 @@ class BoardsController < ApplicationController
     Board.create(board_params)
   end
 
+  def show
+    @board = Board.find(params[:id])
+  end
+
+
   private
   # このprivateメソッドを呼ぶことでparamsの中にあるboardのキーの中のname、titile、bodyの値のみを取得する
-
   def board_params
     params.require(:board).permit(:name,:title,:body)
   end
