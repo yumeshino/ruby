d39a3ac8666cacbf5b5c98ecf13eec6efa0b5edd
USERNAME <shinozakiyume88@gmail.com>
2022-09-07 11:31:03 +0900

    56. リソースベースルーティングとURL用ヘルパー

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # restfull名書き方
-  get 'boards',to: 'boards#index' 
-  get 'boards/new',to: 'boards#new' 
-  post 'boards',to: 'boards#create' 
-  get 'boards/:id',to: 'boards#show'
+  resources :boards,only: [:index, :new, :create, :show]
 						
 end
