50ce1f189877b9e26b2ce05b1c939a29c3a873fb
USERNAME <shinozakiyume88@gmail.com>
2022-09-12 10:48:14 +0900

    64. モデルのアソシエーションの解説とannotationのgem追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  body       :text(65535)
+#  name       :string(255)
+#  title      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Board < ApplicationRecord
   validates :name, presence: true, length: { maximum: 10 }
   validates :title, presence: true, length: { maximum: 30 }
