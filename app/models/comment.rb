0b5d801975c753fa0ea9803c3546334f1ee1e034
USERNAME <shinozakiyume88@gmail.com>
2022-09-17 15:50:34 +0900

    70. Commentモデルへのバリデーション設定とエラーメッセージの表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -19,4 +19,8 @@
 #
 class Comment < ApplicationRecord
   belongs_to :board
+
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :comment, presence: true, length: { maximum: 1000 }
+
 end
