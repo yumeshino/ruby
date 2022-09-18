b7223a35279eff6fe02255a1796c7029de8a51df
USERNAME <shinozakiyume88@gmail.com>
2022-09-19 00:15:20 +0900

    74. 多対多のアソシエーションの設定

--- vendor/cache/ruby/2.6.0/gems/libv8-8.4.255.0/ext/libv8/extconf.rb
+++
@@ -1,7 +0,0 @@
-require 'mkmf'
-create_makefile('libv8')
-
-require File.expand_path '../location', __FILE__
-location = Libv8::Location::Vendor.new
-
-exit location.install!
--- vendor/cache/ruby/2.6.0/gems/libv8-8.4.255.0/spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-$:.unshift File.expand_path '../../ext/libv8', __FILE__
-$:.unshift File.expand_path '../../lib', __FILE__
-require 'rspec'
-require 'libv8'
