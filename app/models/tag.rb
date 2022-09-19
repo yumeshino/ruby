bbfe3e75e7de899ef998ec1f47b9d31874bde7a8
USERNAME <shinozakiyume88@gmail.com>
2022-09-19 12:13:08 +0900

    75. アソシエーションのdependentオプション

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,6 +8,6 @@
 #  updated_at :datetime         not null
 #
 class Tag < ApplicationRecord
-  has_many :board_tag_relations
+  has_many :board_tag_relations, dependent: :delete_all
   has_many :boards, through: :board_tag_relations
 end
