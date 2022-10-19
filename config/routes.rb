d55dda3e2717bd9790cf6e358dee8f86186553b2
USERNAME <shinozakiyume88@gmail.com>
2022-10-19 09:14:31 +0900

    80. ユーザー認証機能に必要なコントローラ、ビュー、モデルのファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/me'
+
+  get 'home/index'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :boards
