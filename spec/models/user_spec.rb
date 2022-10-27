68e7a7735934d2874ca08870efa5c4ccc3b80660
USERNAME <shinozakiyume88@gmail.com>
2022-10-27 10:26:48 +0900

    93. RSpecのユニットテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)      not null
+#  password_digest :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_name  (name) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
